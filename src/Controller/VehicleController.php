26278b98d8717a7d4ed1665078d7262ff6e8d1f3
Thuan Nguyuen <huythuannguyen@yahoo.co.uk>
2016-01-27 15:34:58 -0800

    add css and template for module

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -21,15 +21,38 @@ class VehicleController extends ControllerBase {
   public function render() {
     $first_vehicle = [
         '#theme' => 'vehicle',
-        '#name'  => 'Honda',
+        '#name'  => 'Honda Accord',
         '#attributes' => [
             'id' => 'best-vehicle',
             'class' => ['vehicle--left', 'clearfix'],
         ],
         '#power' => '2.4',
-        '#mile' => '500000',
+        '#mile' => '500.000',
         '#run' => ['free way', 'local', 'moutain'],
-        
+        '#version' => '2016',
+        '#available' => ['sport', 'sedan', 'suv', 'mini-van'],        
+    ];
+    
+    $second_vehicle = [
+        '#theme' => 'vehicle',
+        '#name'  => 'Honda CRV',
+        '#attributes' => [
+            'id' => 'best-vehicle',
+            'class' => ['vehicle--left', 'clearfix'],
+        ],
+        '#power' => '3.4',
+        '#mile' => '300.000',
+        '#run' => ['free way', 'local', 'moutain', 'water'],
+        '#version' => '2017',
+        '#available' => ['sport', 'sedan', 'suv', 'mini-van'],
+    ];
+    return [
+        '#title' => 'Vehicle Description',
+        '#type' => 'container',
+        '#attached' => ['library' => ['vehicle/description']],
+        'attributes' => ['class' => ['admin']],
+        $first_vehicle,
+        $second_vehicle,
     ];
   }
 }
